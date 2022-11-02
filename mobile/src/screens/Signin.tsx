fca7209c0adae93a07bc7aa21998300353647680
LuisFelipeAP <lusfelipea@gmail.com>
2022-11-02 20:48:59 -0300

    feat(build): add mobile

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Center, Text } from 'native-base'
+import Logo from "../assets/logo.svg";
+
+export function Signin() {
+    return (
+        <Center flex="1" bgColor="gray.900">
+            <Logo width={212} height={40} />
+        </Center>    
+    )
+}
